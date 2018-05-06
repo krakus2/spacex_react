b40fe1184998f03a0857ab3889ca15b0e2050d68
Filip Krakowiak <filip.krakowiak@gmail.com>
2018-05-07 01:20:37 +0200

    bem corrections2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,20 +3,20 @@ import '../styles/Footer.css'
 
 const Footer = (props) => (
   <div className="footer">
-    <div className="footer__footerLeft">
-      <span className="footer__footerLeft__text">
+    <div className="footerLeft">
+      <span className="footerLeft__text">
         FOLLOW SPACEX
       </span>
-      <span className="footer__footerLeft__line">
+      <span className="footerLeft__line">
         |
       </span>
-      <button className="footer__footerLeft__button">TWITTER</button>
-      <button className="footer__footerLeft__button">YOUTUBE</button>
-      <button className="footer__footerLeft__button">FLICKR</button>
-      <button className="footer__footerLeft__button">INSTAGRAM</button>
+      <button className="buttonSocial">TWITTER</button>
+      <button className="buttonSocial">YOUTUBE</button>
+      <button className="buttonSocial">FLICKR</button>
+      <button className="buttonSocial">INSTAGRAM</button>
     </div>
-    <div className="footer__footerRight">
-      <span className="footer__footerRight__companyName">
+    <div className="footerRight">
+      <span className="footerRight__companyName">
         2018 SPACE EXPLORATION TECHNOLOGIES CORP.
       </span>
     </div>
