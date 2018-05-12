fd9ce55217b790b0cdf35116c7795f47d676a101
Filip Krakowiak <filip.krakowiak@gmail.com>
2018-05-12 16:18:14 +0200

    changed filters action

---
+++ src/components/list/ListFilters.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import '../../styles/list/ListFilters.css'
+
+class ListFilters extends Component {
+  
+  sieve = (e) => {
+    this.props.sieve(e.target.name)
+  }
+
+  render(){
+    return(
+        <div className="list__header__filters">
+          <button className="buttonRound" name="all rockets" onClick={this.sieve}>ALL ROCKETS</button>
+          <button className="buttonRound" name="falcon 9" onClick={this.sieve}>FALCON 9</button>
+          <button className="buttonRound" name="falcon 1" onClick={this.sieve}>FALCON 1</button>
+          <button className="buttonRound" name="dragon" onClick={this.sieve}>DRAGON</button>
+        </div>
+    )
+  }
+
+};
+
+export default ListFilters;
--- src/components/list/ListHeader.js
+++ src/components/list/ListHeader.js
@@ -1,12 +1,9 @@
 import React, { Component } from 'react';
 import space_x_logo from '../../assets/space_x_logo_bw_centered.svg';
 import '../../styles/list/ListHeader.css'
+import ListFilters from './ListFilters'
 
 class ListHeader extends Component {
-
-  sieve = (e) => {
-    this.props.sieve(e.target.name)
-  }
   render(){
     return(
       <div className="list__header">
@@ -14,12 +11,7 @@ class ListHeader extends Component {
         <span className="list__header__text">
           LAUNCHES 2018
         </span>
-        <div className="list__header__buttonRow">
-          <button className="buttonRound" name="all rockets" onClick={this.sieve}>ALL ROCKETS</button>
-          <button className="buttonRound" name="falcon 9" onClick={this.sieve}>FALCON 9</button>
-          <button className="buttonRound" name="falcon 1" onClick={this.sieve}>FALCON 1</button>
-          <button className="buttonRound" name="dragon" onClick={this.sieve}>DRAGON</button>
-        </div>
+        <ListFilters sieve={this.props.sieve}/>
       </div>
     )
   }
